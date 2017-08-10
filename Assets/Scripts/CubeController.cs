5b4dd7a80eae2610e7928b74ea7aeb02a9f1cb94
Roman <Fezio97@mail.ru>
2017-08-10 18:22:50 +0400

    Вращение куба

---
+++ Assets/Scripts/CubeController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class CubeController : MonoBehaviour
+{
+
+	private float _desireAngle;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown("right") && !DOTween.IsTweening(transform))
+		{
+			transform.DORotate(new Vector3(transform.eulerAngles.x, transform.eulerAngles.y, transform.eulerAngles.z - 90f), 0.2f);
+		}
+		
+		if (Input.GetKeyDown("left") && !DOTween.IsTweening(transform))
+		{
+			transform.DORotate(new Vector3(transform.eulerAngles.x, transform.eulerAngles.y, transform.eulerAngles.z + 90f), 0.2f);
+		}
+		
+		if (Input.GetKeyDown("up") && !DOTween.IsTweening(transform))
+		{
+			transform.DORotate(new Vector3(transform.eulerAngles.x + 90, transform.eulerAngles.y, transform.eulerAngles.z), 0.2f);
+		}
+		
+		if (Input.GetKeyDown("down") && !DOTween.IsTweening(transform))
+		{
+			transform.DORotate(new Vector3(transform.eulerAngles.x - 90, transform.eulerAngles.y, transform.eulerAngles.z), 0.2f);
+		}
+	}
+}
